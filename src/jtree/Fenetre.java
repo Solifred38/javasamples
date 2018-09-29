77b623d18994274a0fe6d77d3e5e306aef500bfb
Frederic Fromager <fred.fromager@gmail.com>
2018-09-29 10:16:01 +0200

    add nodes in treectl

--- src/jtree/Fenetre.java
+++ src/jtree/Fenetre.java
@@ -1,145 +1,150 @@
 package jtree;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
+//CTRL + SHIFT + O pour générer les imports nécessaires
 public class Fenetre extends JFrame {
+private JTree arbre;
+private DefaultMutableTreeNode racine;
+private DefaultTreeModel model;
+private JButton bouton = new JButton("Ajouter");
 
-	private JTree arbre;
-	private DefaultMutableTreeNode racine;
-	private Panneau panneau = new Panneau();
+public Fenetre(){
+  this.setSize(200, 300);
+  this.setLocationRelativeTo(null);
+  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	public Fenetre() {
-		this.setSize(500, 200);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Les arbres");
-		try {
-			// On force à utiliser le « look and feel » du système
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			// Ici on force tous les composants de notre fenêtre (this) à se redessiner avec
-			// le « look and feel » du système
-			SwingUtilities.updateComponentTreeUI(this);
-		} catch (InstantiationException e) {
-		} catch (ClassNotFoundException e) {
-		} catch (UnsupportedLookAndFeelException e) {
-		} catch (IllegalAccessException e) {
-		}
-		// On invoque la méthode de construction de notre arbre
-		listRoot();
+  this.setTitle("JTree");
+  //On invoque la méthode de construction de l'arbre
 
-		this.setVisible(true);
-	}
+  listRoot();
+  bouton.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent event) {
+      if(arbre.getLastSelectedPathComponent() != null){                  
+        JOptionPane jop = new JOptionPane();
+        String nodeName = jop.showInputDialog("Saisir un nom de noeud");
+             
+        if(nodeName != null && !nodeName.trim().equals("")){                  
+          DefaultMutableTreeNode parentNode = (DefaultMutableTreeNode)arbre.getLastSelectedPathComponent();
+          DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(nodeName);
+          parentNode.add(childNode);
+          model.insertNodeInto(childNode, parentNode, parentNode.getChildCount()-1);
+          model.nodeChanged(parentNode);                  
+        }               
+      }
+      else{
+        System.out.println("Aucune sélection !");
+      }
+    }
+  });
+  this.getContentPane().add(bouton, BorderLayout.SOUTH);
+  this.setVisible(true);
+}
+ 
+private void listRoot(){
+  this.racine = new DefaultMutableTreeNode();
 
-	private void listRoot() {
+  int count = 0;
+  for(File file : File.listRoots())
+  {
+    DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file.getAbsolutePath());
+    try {
+      for(File nom : file.listFiles()){
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom.getName()+"\\");               
+        lecteur.add(this.listFile(nom, node));               
+      }
+    } catch (NullPointerException e) {}
+       
+    this.racine.add(lecteur);         
+  }
+  //Nous créons, avec notre hiérarchie, un arbre
+  arbre = new JTree();
+  this.model = new DefaultTreeModel(this.racine);      
+  arbre.setModel(model);
+  arbre.setRootVisible(false);
+  arbre.setEditable(true);
+  arbre.getModel().addTreeModelListener(new TreeModelListener() {
+    public void treeNodesChanged(TreeModelEvent evt) {
+      System.out.println("Changement dans l'arbre");
+      Object[] listNoeuds = evt.getChildren();
+      int[] listIndices = evt.getChildIndices();
+      for (int i = 0; i < listNoeuds.length; i++) {
+        System.out.println("Index " + listIndices[i] + ", noeud déclencheur : " + listNoeuds[i]);
+      }
+    }   
+    public void treeNodesInserted(TreeModelEvent event) {
+      System.out.println("Un noeud a été inséré !");            
+    }
+    public void treeNodesRemoved(TreeModelEvent event) {
+      System.out.println("Un noeud a été retiré !");
+    }
+    public void treeStructureChanged(TreeModelEvent event) {
+      System.out.println("La structure d'un noeud a changé !");
+    }
+  });
 
-		this.racine = new DefaultMutableTreeNode();
+  this.getContentPane().add(new JScrollPane(arbre), BorderLayout.CENTER);
+}
 
-		int count = 0;
-		for (File file : File.listRoots()) {
-			DefaultMutableTreeNode lecteur = new DefaultMutableTreeNode(file.getAbsolutePath());
-			try {
-				for (File nom : file.listFiles()) {
-					DefaultMutableTreeNode node = new DefaultMutableTreeNode(nom.getName() + "\\");
-					lecteur.add(this.listFile(nom, node));
-				}
-			} catch (NullPointerException e) {
-			}
+private DefaultMutableTreeNode listFile(File file, DefaultMutableTreeNode node){
+  int count = 0;      
+  if(file.isFile())
+    return new DefaultMutableTreeNode(file.getName());
+  else{
+    File[] list = file.listFiles();
+    if(list == null)
+      return new DefaultMutableTreeNode(file.getName());
 
-			this.racine.add(lecteur);
+    for(File nom : list){
+      count++;
+      //Pas plus de 5 enfants par noeud
+      if(count < 3){
+        DefaultMutableTreeNode subNode;
+        if(nom.isDirectory()){
+          subNode = new DefaultMutableTreeNode(nom.getName()+"\\");
+          node.add(this.listFile(nom, subNode));
+        }else{
+          subNode = new DefaultMutableTreeNode(nom.getName());
+        }
+        node.add(subNode);
+      }
+    }
+    return node;
+  }
+}
 
-			// Si nous avons parcouru plus de 50 dossiers, on sort
-			// if(count > 50) {break;}
+public static void main(String[] args){
+  //Nous allons créer des fenêtres avec des look and feel différents 
+  //Cette instruction permet de récupérer tous les look and feel du système
 
-		}
-		// On crée, avec notre hiérarchie, un arbre
-		arbre = new JTree(this.racine);
-		arbre.setRootVisible(false);
-		arbre.addTreeSelectionListener(new TreeSelectionListener() {
-			public void valueChanged(TreeSelectionEvent event) {
-				if (arbre.getLastSelectedPathComponent() != null) {
-					// La méthode getPath retourne un objet TreePath
-					File file = new File(getAbsolutePath(event.getPath()));
-					panneau.setTexte(getDescription(file));
-				}
-			}
+  try {
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+  }
+  catch (InstantiationException e) {}
+  catch (ClassNotFoundException e) {}
+  catch (UnsupportedLookAndFeelException e) {}
+  catch (IllegalAccessException e) {}
+  Fenetre fen = new Fenetre();
 
-			private String getAbsolutePath(TreePath treePath) {
-				String str = "";
-				// On balaie le contenu de notre TreePath
-				for (Object name : treePath.getPath()) {
-					// Si l'objet à un nom, on l'ajoute au chemin
-					if (name.toString() != null)
-						str += name.toString();
-				}
-				return str;
-			}
-
-			/**
-			 * Retourne une description d'un objet File
-			 * 
-			 * @param file
-			 * @return
-			 */
-			private String getDescription(File file) {
-				String str = "Chemin d'accès sur le disque : \n\t" + file.getAbsolutePath() + "\n";
-				str += (file.isFile()) ? "Je suis un fichier.\nJe fais " + file.length() + " ko\n"
-						: "Je suis un dossier.\n";
-				str += "J'ai des droits : \n";
-				str += "\t en lecture : " + ((file.canRead()) ? "Oui;" : "Non;");
-				str += "\n\t en écriture : " + ((file.canWrite()) ? "Oui;" : "Non;");
-				return str;
-			}
-		});
-		// On crée un séparateur de conteneur pour réviser
-		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(arbre),
-				new JScrollPane(panneau));
-		// On place le séparateur
-		split.setDividerLocation(200);
-		// On ajoute le tout
-		this.getContentPane().add(split, BorderLayout.CENTER);
-	}
-
-	private DefaultMutableTreeNode listFile(File file, DefaultMutableTreeNode node) {
-		int count = 0;
-
-		if (file.isFile())
-			return new DefaultMutableTreeNode(file.getName());
-		else {
-			File[] list = file.listFiles();
-			if (list == null)
-				return new DefaultMutableTreeNode(file.getName());
-			for (File nom : file.listFiles()) {
-				count++;
-				// Pas plus de 5 enfants par nœud
-				if (count < 5) {
-					DefaultMutableTreeNode subNode;
-					if (nom.isDirectory()) {
-						subNode = new DefaultMutableTreeNode(nom.getName() + "\\");
-						node.add(this.listFile(nom, subNode));
-					} else {
-						subNode = new DefaultMutableTreeNode(nom.getName());
-					}
-					node.add(subNode);
-				}
-			}
-			return node;
-		}
-	}
-
-	public static void main(String[] args) {
-		Fenetre fen = new Fenetre();
-	}
+}
 }
