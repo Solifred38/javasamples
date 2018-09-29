84814babdbcd3ed60716f7061eaab2969660af6a
Frederic Fromager <fred.fromager@gmail.com>
2018-09-29 13:20:09 +0200

    table model

--- src/tableau/Fenetre.java
+++ src/tableau/Fenetre.java
@@ -1,35 +1,70 @@
 package tableau;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
+//CTRL + SHIFT + O pour générer les imports
 public class Fenetre extends JFrame {
 
-  public Fenetre(){
-    this.setLocationRelativeTo(null);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setTitle("JTable");
-    this.setSize(300, 120);
+private JTable tableau;
+private JButton change = new JButton("Changer la taille");
 
-    //Les données du tableau
-    Object[][] data = {
-      {"Cysboy", "28 ans", "1.80 m"},
-      {"BZHHydde", "28 ans", "1.80 m"},
-      {"IamBow", "24 ans", "1.90 m"},
-      {"FunMan", "32 ans", "1.85 m"}
-    };
+public Fenetre(){
+  this.setLocationRelativeTo(null);
+  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  this.setTitle("JTable");
+  this.setSize(600, 140);
 
-    //Les titres des colonnes
-    String  title[] = {"Pseudo", "Age", "Taille"};
-    JTable tableau = new JTable(data, title);
-    //Nous ajoutons notre tableau à notre contentPane dans un scroll
-    //Sinon les titres des colonnes ne s'afficheront pas !
-    this.getContentPane().add(new JScrollPane(tableau));
-  }   
+  Object[][] data = {   
+    {"Cysboy", new JButton("6boy"), new Double(1.80), new Boolean(true)},
+    {"BZHHydde", new JButton("BZH"), new Double(1.78), new Boolean(false)},
+    {"IamBow", new JButton("BoW"), new Double(1.90), new Boolean(false)},
+    {"FunMan", new JButton("Year"), new Double(1.85), new Boolean(true)}
+  };
 
-  public static void main(String[] args){
-    Fenetre fen = new Fenetre();
-    fen.setVisible(true);
-  }   
+  String  title[] = {"Pseudo", "Age", "Taille", "OK ?"};
+
+  ZModel model = new ZModel(data, title);
+  System.out.println("Nombre de colonne : " + model.getColumnCount());
+  System.out.println("Nombre de ligne : " + model.getRowCount());
+  this.tableau = new JTable(model);
+  this.getContentPane().add(new JScrollPane(tableau), BorderLayout.CENTER);      
+}
+
+//Classe modèle personnalisée
+class ZModel extends AbstractTableModel{
+  private Object[][] data;
+  private String[] title;
+
+  //Constructeur
+  public ZModel(Object[][] data, String[] title){
+    this.data = data;
+    this.title = title;
+  }
+
+  //Retourne le nombre de colonnes
+  public int getColumnCount() {
+    return this.title.length;
+  }
+
+  //Retourne le nombre de lignes
+  public int getRowCount() {
+    return this.data.length;
+  }
+
+  //Retourne la valeur à l'emplacement spécifié
+  public Object getValueAt(int row, int col) {
+    return this.data[row][col];
+  }            
+}
+
+public static void main(String[] args){
+  Fenetre fen = new Fenetre();
+  fen.setVisible(true);
+}
 }
