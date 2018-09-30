ebc729e5cdc054433ff0f547fc3be259529e57ad
Frederic Fromager <fred.fromager@gmail.com>
2018-09-30 11:36:04 +0200

    gestion complet tableau avec model

--- src/tableau/Fenetre.java
+++ src/tableau/Fenetre.java
@@ -17,99 +17,70 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 
-//CTRL + SHIFT + O pour générer les imports
 public class Fenetre extends JFrame {
-
-private JTable tableau;
-private JButton change = new JButton("Changer la taille");
-//Contenu de notre combo
-private String[] comboData = {"Très bien", "Bien", "Mal"};
-
-public Fenetre(){
-  this.setLocationRelativeTo(null);
-  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  this.setTitle("JTable");
-  this.setSize(600, 180);
-  //Données de notre tableau
-  Object[][] data = {   
-    {"Cysboy", "6boy", comboData[0], new Boolean(true)},
-    {"BZHHydde", "BZH", comboData[0], new Boolean(false)},
-    {"IamBow", "BoW", comboData[0], new Boolean(false)},
-    {"FunMan", "Year", comboData[0], new Boolean(true)}
-  };
-  //Titre du tableau
-  String  title[] = {"Pseudo", "Age", "Taille", "OK ?"};
-  //Combo à utiliser
-  JComboBox combo = new JComboBox(comboData);
-
-  this.tableau = new JTable(data, title);      
-  this.tableau.setRowHeight(30);
-  this.tableau.getColumn("Age").setCellRenderer(new ButtonRenderer());
-  this.tableau.getColumn("Age").setCellEditor(new ButtonEditor(new JCheckBox()));
-  //On définit l'éditeur par défaut pour la cellule en lui spécifiant quel type d'affichage prendre en compte
-  this.tableau.getColumn("Taille").setCellEditor(new DefaultCellEditor(combo));
-  this.getContentPane().add(new JScrollPane(tableau), BorderLayout.CENTER);
-}
-public class ButtonRenderer extends JButton implements TableCellRenderer{
-
-	  public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean isFocus, int row, int col) {
-	    //On écrit dans le bouton ce que contient la cellule
-	    setText((value != null) ? value.toString() : "");
-	    //On retourne notre bouton
-	    return this;
-	  }
+	 
+	   private JTable tableau;
+	   private JButton change = new JButton("Changer la taille");
+	   private String[] comboData = {"Très bien", "Bien", "Mal"};
+	   private String supp = "Supprimer la ligne";
+	   private JComboBox combo;
+	    
+	   public Fenetre(){
+	      this.setLocationRelativeTo(null);
+	      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      this.setTitle("JTable");
+	      this.setSize(600, 250);
+	      this.createContent();
+	   }
+	    
+	   private void createContent(){
+	      //Données de notre tableau
+	      Object[][] data = {  
+	         {"Cysboy", "6boy", comboData[0], new Boolean(true), supp},
+	         {"BZHHydde", "BZH", comboData[0], new Boolean(false), supp},
+	         {"IamBow", "BoW", comboData[0], new Boolean(false), supp},
+	         {"FunMan", "Year", comboData[0], new Boolean(true), supp}
+	      };
+	 
+	      String  title[] = {"Pseudo", "Age", "Taille", "OK ?", "Suppression"};
+	      combo = new JComboBox(comboData);
+	       
+	      //Notre modèle d'affichage spécifique destiné à pallier
+	      //les bugs d'affichage !
+	      ZModel zModel = new ZModel(data, title);
+	       
+	      this.tableau = new JTable(zModel);     
+	      this.tableau.setRowHeight(30);
+	      this.tableau.getColumn("Age").setCellRenderer(new ButtonRenderer());
+	      this.tableau.getColumn("Age").setCellEditor(new ButtonEditor(new JCheckBox()));
+	       
+	      //On définit l'éditeur par défaut pour la cellule
+	      //en lui spécifiant quel type d'affichage prendre en compte
+	      this.tableau.getColumn("Taille").setCellEditor(new DefaultCellEditor(combo));
+	      DefaultTableCellRenderer dcr = new DefaultTableCellRenderer();
+	      this.tableau.getColumn("Taille").setCellRenderer(dcr);
+	       
+	      //On définit un éditeur pour la colonne "supprimer"
+	      this.tableau.getColumn("Suppression").setCellEditor(new DeleteButtonEditor(new JCheckBox()));
+	       
+	      //On ajoute le tableau
+	      this.getContentPane().add(new JScrollPane(tableau), BorderLayout.CENTER);
+	       
+	      JButton ajouter = new JButton("Ajouter une ligne");
+	      ajouter.addActionListener(new MoreListener());
+	      this.getContentPane().add(ajouter, BorderLayout.SOUTH);
+	   }     
+	    
+	   class MoreListener implements ActionListener{
+	      public void actionPerformed(ActionEvent event) {
+	         Object[] donnee = new Object[]
+	            {"Angelo", "Rennais", comboData[0], new Boolean(false), supp};
+	         ((ZModel)tableau.getModel()).addRow(donnee);
+	      }
+	   }
+	       
+	   public static void main(String[] args){
+	      Fenetre fen = new Fenetre();
+	      fen.setVisible(true);
+	   }
 	}
-public class ButtonEditor extends DefaultCellEditor {
-
-	  protected JButton button;
-	  private boolean   isPushed;
-	  private ButtonListener bListener = new ButtonListener();
-	   
-	  //Constructeur avec une CheckBox
-	  public ButtonEditor(JCheckBox checkBox) {
-	    //Par défaut, ce type d'objet travaille avec un JCheckBox
-	    super(checkBox);
-	    //On crée à nouveau un bouton
-	    button = new JButton();
-	    button.setOpaque(true);
-	    //On lui attribue un listener
-	    button.addActionListener(bListener);
-	  }
-
-	  public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) { 
-	    //On précise le numéro de ligne à notre listener
-	    bListener.setRow(row);
-	    //Idem pour le numéro de colonne
-	    bListener.setColumn(column);
-	    //On passe aussi le tableau en paramètre pour des actions potentielles
-	    bListener.setTable(table);
-	      
-	    //On réaffecte le libellé au bouton
-	    button.setText( (value == null) ? "" : value.toString() );
-	    //On renvoie le bouton
-	    return button;
-	  }
-	   
-	  //Notre listener pour le bouton
-	  class ButtonListener implements ActionListener{        
-	    private int column, row;
-	    private JTable table;
-	    private int nbre = 0;
-	        
-	    public void setColumn(int col){this.column = col;}
-	    public void setRow(int row){this.row = row;}
-	    public void setTable(JTable table){this.table = table;}
-	        
-	    public void actionPerformed(ActionEvent event) {
-	      //On affiche un message, mais vous pourriez effectuer les traitements que vous voulez
-	      System.out.println("coucou du bouton : " + ((JButton)event.getSource()).getText());
-	      //On affecte un nouveau libellé à une autre cellule de la ligne
-	      table.setValueAt("New Value " + (++nbre), this.row, (this.column -1));
-	    }
-	  }     
-	}
-public static void main(String[] args){
-  Fenetre fen = new Fenetre();
-  fen.setVisible(true);
-}
-}
